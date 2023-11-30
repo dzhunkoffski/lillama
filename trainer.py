4596dc0e28bcd2a6bb0f9ce36db3d675e0802a29
Kirill Dzhunkovskiy <dzhunkofffski@gmail.com>
2023-11-30 17:37:50 +0300

    asdasfda

--- trainer.py
+++ trainer.py
@@ -46,8 +46,9 @@ def training_epoch(model, optimzier, criterion, train_loader, device, tqdm_desc,
         
         if (i+1) % grad_accum == 0 or (i+1) == len(train_loader):
             optimzier.step()
+        log.log_state("grad_norm", get_grad_norm(model))
+        if (i+1) % grad_accum == 0 or (i+1) == len(train_loader):
             optimzier.zero_grad()
-            log.log_state("grad_norm", get_grad_norm(model))
 
         train_loss += loss.item() * indices.shape[0]
 
